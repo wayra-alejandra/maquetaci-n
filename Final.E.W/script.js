ef216dec739002b04077c00955a800fbf1c995aa
wayra-alejandra <wayra.carrascalvi@amigo.edu.co>
2024-11-05 14:21:22 -0500

    Add files via upload

---
+++ Final.E.W/script.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links'); // Selecciona el contenedor del menú
+    navLinks.classList.toggle('show'); // Alterna la clase 'show' para mostrar/ocultar el menú
+}
